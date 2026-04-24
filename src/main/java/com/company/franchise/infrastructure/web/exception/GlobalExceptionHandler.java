acd21803ac979507b499b5b7988a702d89015421
kiyota <c4mo.sw@gmail.com>
2026-04-24 11:35:08 -0500

    feat(test): Not found exception implemented

---
+++ src/main/java/com/company/franchise/infrastructure/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.company.franchise.infrastructure.web.exception;
+
+public class GlobalExceptionHandler {
+}
---
+++ src/test/java/com/company/franchise/BaseIntegrationTest.java
@@ -0,0 +1,4 @@
+package com.company.franchise;
+
+public class BaseIntegrationTest {
+}
