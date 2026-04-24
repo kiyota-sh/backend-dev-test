80e98fe3897a22a0b581f1b7e0253cdeb9c7aba4
kiyota <c4mo.sw@gmail.com>
2026-04-23 23:59:50 -0500

    feat(init): Project initialization

---
+++ src/test/java/com/company/franchise/FranchiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
