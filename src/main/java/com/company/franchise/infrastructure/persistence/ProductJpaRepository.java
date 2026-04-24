38c533cdb65a9098721279c87325f73ee76bbeae
kiyota <c4mo.sw@gmail.com>
2026-04-24 11:38:04 -0500

    feat(query): Get max estock products

--- src/main/java/com/company/franchise/infrastructure/persistence/ProductJpaRepository.java
+++ src/main/java/com/company/franchise/infrastructure/persistence/ProductJpaRepository.java
@@ -1,5 +1,20 @@
 package com.company.franchise.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {}
+import java.util.List;
+
+public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
+    @Query("""
+            SELECT p FROM ProductEntity p
+            WHERE p.branch.franchise.id = :franchiseId
+            AND p.stock = (
+                SELECT MAX(p2.stock)
+                FROM ProductEntity p2
+                WHERE p2.branch.id = p.branch.id
+            )
+        """)
+    List<ProductEntity> findTopProductsByFranchise(@Param("franchiseId") Long franchiseId);
+}
